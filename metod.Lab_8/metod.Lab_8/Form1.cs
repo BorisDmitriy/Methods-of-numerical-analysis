2a91b308ed42d8d0de3969a4564307ad7baf934c
Boris Dmitriy <BorisDmitriy@users.noreply.github.com>
2018-05-11 14:37:54 +0300

    Correction in the lab 8
    In the previous version, the place of the parabolic spline was cubic, which was not required

--- metod.Lab_8/metod.Lab_8/Form1.cs
+++ metod.Lab_8/metod.Lab_8/Form1.cs
@@ -19,11 +19,11 @@ namespace metod.Lab_8
 
 			chart1.Series.Add("Сплайн");
 			chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+		
+            chart1.Series.Add("Параболическая интерполяция сплайна");
+            chart1.Series[2].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
 
-			chart1.Series.Add("Интрерполяция");
-			chart1.Series[2].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline ;
-
-			double[] MasX = new double[] { 0.55, 0.8, 0.93, 1.16, 1.31, 1.48, 1.78, 2.06, 2.56, 2.89 };
+            double[] MasX = new double[] { 0.55, 0.8, 0.93, 1.16, 1.31, 1.48, 1.78, 2.06, 2.56, 2.89 };
 			double[] MasY = new double[] {0.44, 0.8, 1.63, 1.26, 1.07, 1.11, 0.83, 0.37, 0.21, 0.15};
 			
 			chart1.Series[0].Points.DataBindXY(MasX, MasY);
@@ -32,20 +32,20 @@ namespace metod.Lab_8
 
 			List<double> X = new List<double>();
 			List<double> Y = new List<double>();
-
-			spline s = new spline();
-
-			s.BuildSpline(MasX, MasY, MasX.Length);
-			for ( var x = MasX[0] ; x <= MasX[MasX.Length-1]; x += 0.1)
+			
+            ParabSpline ps = new ParabSpline();
+			
+			for ( var x = MasX[0] ; x <= MasX[MasX.Length-1]; x += 0.001)
 			{
 				X.Add(x);
-				Y.Add(s.Interpolate(x));
-			}
+                Y.Add(ps.build_spline_and_interpolate(x, MasX, MasY));
+            }
+
 			chart1.Series[2].Points.DataBindXY(X, Y);
 
-
-			chart1.Series[0].Color = Color.Green;
+            chart1.Series[0].Color = Color.Green;
 			chart1.Series[0].BorderWidth = 7;
+			chart1.Series[2].Color = Color.DarkBlue;
 		}
 	}
 }
---
+++ metod.Lab_8/metod.Lab_8/ParabSpline.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metod.Lab_8
+{
+	class ParabSpline
+	{
+		// Вычисление значения интерполированной функции в произвольной точке
+		public double build_spline_and_interpolate(double x, double[] arraX, double[] arrayY)
+		{
+			List<double> a = new List<double>();
+			List<double> z = new List<double>();
+			a.AddRange(arrayY);
+			for ( int i = 0 ; i < arrayY.Length-1 ; i++ )
+			{
+				z.Add(2 * ( arrayY[i + 1] - arrayY[i] ) /
+						  ( arraX[i + 1] - arraX[i] ));
+			}
+			List<double> b = new List<double>();
+			b.Add( a[0]);
+
+			for ( int i = 1 ; i < arrayY.Length ; i++ )
+			{
+				b.Add( z[i - 1] - b[i - 1]);
+			}
+
+			List<double> c = new List<double>();
+
+			for ( int i = 0 ; i < arrayY.Length-1 ; i++ )
+			{
+				c.Add(( b[i + 1] - b[i] ) /
+					   ( 2 * ( arraX[i + 1] - arraX[i] ) ));
+			}
+			
+			//производим бинарный поиск нужного эл-та массива
+			int j = 0;
+			int id = arraX.Length - 1;
+			while ( j + 1 < id )
+			{
+				int k = j + ( id - j ) / 2;
+				if ( x <= arraX[k] )
+				{
+					id = k;
+				}
+				else
+				{
+					j = k;
+				}
+			}
+
+			if ( x < arraX[id] )
+				return a[id - 1] + ( x - arraX[id - 1] ) * b[id - 1] + c[id - 1] * ( x - arraX[id - 1] ) * ( x - arraX[id - 1] );
+			else
+				return a[id];
+			
+		}
+	}
+}
+
